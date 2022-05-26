1e1a39b488432315d4110488d8d5a51e871f51d2
FruitJuic3 <dragonfruitgamesinc@gmail.com>
2022-05-26 16:16:25 -0700

    Removed mirror screen for now

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import classes
 import inputs
 import screens
 import inventory
-import mirrorscreens
 
 alive = True
 
@@ -12,15 +11,10 @@ classManager = classes.ClassManager()
 entityManager = entities.EntityManager()
 inventoryManager = inventory.InventoryManager(entityManager)
 screenManager = screens.ScreenManager(inputManager, classManager, entityManager, inventoryManager)
-mirrorScreenManager = mirrorscreens.ScreenManager(inputManager, classManager, entityManager, inventoryManager)
 
 screenManager.runScreen("Main Menu")
 
 while alive:
     player = entityManager.player
     screenManager.clear()
-    if player.mirrored:
-        player.currentScreen = mirrorScreenManager.runScreen(player.currentScreen)
-    else:
-        player.currentScreen = screenManager.runScreen(player.currentScreen)
-        
+    player.currentScreen = screenManager.runScreen(player.currentScreen)    
